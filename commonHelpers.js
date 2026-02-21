4a71e88f17c5b7d91a16b5939fe3ec599309ac2c
Victoriia <v.denysova@student.bachelor-goit.com.ua>
2026-02-21 13:03:11 +0200

    Updates

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import{i as o,a as t,_ as a}from"./assets/applyAssets-6e37b008.js";o().then(()=>(t(),a(()=>import("./assets/favorites-59f5abb5.js"),["assets/favorites-59f5abb5.js","assets/exercises-right-part-filter-55757d4f.js","assets/vendor-295897c2.js"]))).catch(r=>console.error("bootstrap-favorites error:",r));
+import{i as o,a as t,_ as a}from"./assets/applyAssets-2894e2f3.js";o().then(()=>(t(),a(()=>import("./assets/favorites-59f5abb5.js"),["assets/favorites-59f5abb5.js","assets/exercises-right-part-filter-55757d4f.js","assets/vendor-295897c2.js"]))).catch(r=>console.error("bootstrap-favorites error:",r));
 //# sourceMappingURL=commonHelpers.js.map
--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import{i as o,a as t,_ as a}from"./assets/applyAssets-6e37b008.js";o().then(()=>(t(),a(()=>import("./assets/main-63d79abc.js"),["assets/main-63d79abc.js","assets/exercises-right-part-filter-55757d4f.js","assets/vendor-295897c2.js"]))).catch(r=>console.error("bootstrap error:",r));
+import{i as o,a as t,_ as a}from"./assets/applyAssets-2894e2f3.js";o().then(()=>(t(),a(()=>import("./assets/main-63d79abc.js"),["assets/main-63d79abc.js","assets/exercises-right-part-filter-55757d4f.js","assets/vendor-295897c2.js"]))).catch(r=>console.error("bootstrap error:",r));
 //# sourceMappingURL=commonHelpers2.js.map
